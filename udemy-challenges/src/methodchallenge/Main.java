e6ebcc18d853dded53b59f4133aa33f6172744c8
Megan Miller <megan.miller@smoldev.dev>
2023-05-07 18:20:43 -0400

    remove multiple returns.

--- udemy-challenges/src/methodchallenge/Main.java
+++ udemy-challenges/src/methodchallenge/Main.java
@@ -31,17 +31,18 @@ public class Main
 
     public static int calculateHighScorePosition(int score)
     {
+        int position = 4;
         if(score >= 1000)
         {
-            return 1;
+            position = 1;
         } else if(score >= 500) {
-            return 2;
+            position = 2;
         } else if(score >= 100)
         {
-            return 3;
+            position = 3;
         }
 
-        return 4;
+        return position;
     }
 
 
