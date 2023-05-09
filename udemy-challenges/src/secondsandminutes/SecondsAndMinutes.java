e8236b90fc771493df2029a24368e5df81156792
Megan Miller <megan.miller@smoldev.dev>
2023-05-09 15:33:31 -0400

    Seconds to Minutes challenge -- small fix

--- udemy-challenges/src/secondsandminutes/SecondsAndMinutes.java
+++ udemy-challenges/src/secondsandminutes/SecondsAndMinutes.java
@@ -23,7 +23,7 @@ public class SecondsAndMinutes
 
     public static String getDurationString(int minutes, int seconds)
     {
-        if(minutes < 0 && seconds <= 0 || seconds >= 59)
+        if(minutes < 0 || (seconds <= 0 || seconds >= 59))
         {
             return "Invalid Values";
         }
