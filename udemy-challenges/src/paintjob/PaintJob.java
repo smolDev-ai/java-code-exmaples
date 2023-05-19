44041be06180bd73d725b974479d5265f573943c
smoldev-ai <megan.miller@smoldev.dev>
2023-05-18 20:17:49 -0400

    Paint Job Challenge

---
+++ udemy-challenges/src/paintjob/PaintJob.java
@@ -0,0 +1,41 @@
+package paintjob;
+
+public class PaintJob
+{
+
+    public static int getBucketCount(double area, double areaPerBucket)
+    {
+        if(area <= 0 || areaPerBucket <= 0)
+        {
+            return -1;
+        }
+
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket)
+    {
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0)
+        {
+            return -1;
+        }
+
+        return (int) Math.ceil((width * height) / areaPerBucket);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets)
+    {
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0)
+        {
+            return -1;
+        }
+
+        return (int) Math.ceil(((width * height) / areaPerBucket) - extraBuckets);
+    }
+
+
+    public static void main(String[] args)
+    {
+        System.out.println(getBucketCount(0.0,1.5));
+    }
+}
